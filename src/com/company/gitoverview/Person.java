91619786128f075f4e95c19f4668ccb05e1d276b
Tigran Poghosyan <tigran.poghosyan.2000@mail.ru>
2021-03-08 20:23:44 +0400

    My another commit

--- src/com/company/gitoverview/Person.java
+++ src/com/company/gitoverview/Person.java
@@ -1,8 +1,24 @@
 package com.company.gitoverview;
 
+import java.util.Objects;
+
 public class Person extends BaseEntity {
     private String firstName;
     private String className;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(firstName, person.firstName) &&
+                Objects.equals(className, person.className);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, className);
+    }
 
     @Override
     public String toString() {
